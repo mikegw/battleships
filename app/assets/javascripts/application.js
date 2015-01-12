a62d6ea95292c3bcb7614a56daffb5eb680f7603
mikegw <mikewilliamson86@gmail.com>
2015-01-12 22:32:24 +0000

    remove unnecessary require_trees

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,9 +22,7 @@
 //= require eventBus
 //= require_tree ../templates
 //= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
 
 

01c7b43ecedd8ebb2da665a9eae2ae8a5f3c78ae
mikegw <mikewilliamson86@gmail.com>
2015-01-13 15:29:50 +0000

    pusher issue

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,4 @@
 Pusher.app_id = ENV["PUSHER_APP_ID"]
 Pusher.key = ENV["PUSHER_KEY"]
 Pusher.secret = ENV["PUSHER_SECRET"]
-Pusher.host = "api-eu.pusher.com"
 Pusher.url = "http://#{Pusher.key}:#{Pusher.secret}@#{Pusher.host}/apps/#{Pusher.app_id}"
